0e63797d29da56d78f29d22502e6dfbb49eb3415
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-03 18:50:36 -0400

    listening for post requests under create_lobby endpoint

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
+
+const express = require("express")
 const path = require("path")
 
 module.exports = (app) => {
+  app.use(express.json())
+  app.use(express.raw())
+  app.use(express.urlencoded({ extended: true }))
+
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
   })
@@ -10,7 +16,8 @@ module.exports = (app) => {
   })
 
 
-  app.get("/:path", (req, res) => {
-    res.sendFile(path.join(__dirname, `/public/${req.params.path}`))
+  app.post("/create_lobby", (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
   })
 }
