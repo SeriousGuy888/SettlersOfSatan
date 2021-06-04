f709f392738ab0e886e00592765c416909d0828e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-03 22:08:52 -0400

    route for lobby page

--- server/routes.js
+++ server/routes.js
@@ -9,13 +9,18 @@ module.exports = (app) => {
   app.use(express.urlencoded({ extended: true }))
 
   const cwd = process.cwd() // directory of where server.js is
+  const publicDir = path.join(cwd, "/public")
 
   app.get("/", (req, res) => {
-    res.sendFile(path.join(cwd, "/public/index.html"))
+    res.sendFile(path.join(publicDir, "/index.html"))
+  })
+
+  app.get("/lobby", (req, res) => {
+    res.sendFile(path.join(publicDir, "/lobby.html"))
   })
 
   app.get("/:path", (req, res) => {
-    res.sendFile(path.join(cwd, `/public/${req.params.path}`))
+    res.sendFile(path.join(publicDir, `/${req.params.path}`))
   })
 
 
