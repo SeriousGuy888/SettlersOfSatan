f3f2518794760d042200f4f19dfa166604255112
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-03 22:03:28 -0400

    sockets properly implemented now :O

--- server.js
+++ server.js
@@ -1,53 +1,26 @@
-const express = require("express")
 const http = require("http")
-const WebSocket = require("ws")
+const socketIO = require("socket.io")
 
+const express = require("express")
 const app = express()
 const port = 3000
 require("./server/routes.js")(app)
 
 const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
-
-// https://medium.com/factory-mind/websocket-node-js-express-step-by-step-using-typescript-725114ad5fe4
-wss.on("connection", ws => {
-  ws.isAlive = true
-  ws.on("pong", () => ws.isAlive = true)
-
-  ws.on("message", message => {
-    console.log(`received ${message} from websocket`)
-
-    const broadcastRegex = /^broadcast\:/
-    if(broadcastRegex.test(message)) {
-      message = message.replace(broadcastRegex, "")
-
-      //send back the message to the other clients
-      wss.clients.forEach(client => {
-        if(client !== ws) {
-          client.send(`Hello, broadcast message -> ${message}`)
-        }    
-      })
-    }
-    else {
-      ws.send(`Hello, you sent -> ${message}`)
-    }
-  })
-
-  ws.send("You've connected to a websocket!")
-})
-
-setInterval(() => { // nuke inactive websockets
-  wss.clients.forEach(ws => {
-    if(!ws.isAlive) return ws.terminate()
-
-    ws.isAlive = false
-    ws.ping(null, false, true)
-  })
-}, 10000)
-
-
-
+const io = socketIO(server)
 
 server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port} | ws://localhost:${port}`)
+  console.log(`Listening at http://localhost:${port}`)
+})
+
+io.on("connection", socket => { // https://dev.to/asciiden/how-to-use-socket-io-not-the-chat-3l21
+  console.log("A user has connected to via a socket.")
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected.")
+  })
+
+  socket.on("oeuf", data => {
+    io.emit("egg", data)
+    console.log("oeuf")
+  })
 })
