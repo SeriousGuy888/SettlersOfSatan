41253277590545ae8a8c745d09bff7c8ba3a0084
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-03 22:29:05 -0400

    added a lobby class

---
+++ classes/Lobby.js
@@ -0,0 +1,43 @@
+class Lobby {
+  constructor(name) {
+    let lobbyCode = ""
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for(let i = 0; i < 5; i++) {
+      lobbyCode += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+    this.name = name
+    this.maxPlayers = 6
+    this.code = lobbyCode
+    this.users = new Set()
+  }
+
+  isFull() {
+    return this.users.size >= this.maxPlayers
+  }
+
+  hasUser(userId) {
+    return this.users.has(userId)
+  }
+
+  join(userId) {
+    if(this.isFull()) return false
+    if(this.hasUser(userId)) return false
+
+    this.users.add(userId)
+    return true
+  }
+
+  leave(userId) {
+    if(this.hasUser(userId)) {
+      this.users.delete(userId)
+    }
+    return false
+  }
+
+  getCode() {
+    return this.code
+  }
+}
+
+module.exports = Lobby
