9cf2bd9054e3735ee99f71caa91ec49e549f9f57
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-04 00:27:07 -0400

    create lobby and join lobby buttons

---
+++ public/lobby.js
@@ -0,0 +1,16 @@
+const lobbyPanel = document.querySelector("#lobby-panel")
+const createLobbyPanel = document.querySelector("#create-lobby-panel")
+const createLobbyNameInput = document.querySelector("#create-lobby-name-input")
+const createLobbyButton = document.querySelector("#create-lobby-button")
+const joinLobbyPanel = document.querySelector("#join-lobby-panel")
+const joinLobbyCodeInput = document.querySelector("#join-lobby-code-input")
+const joinLobbyButton = document.querySelector("#join-lobby-button")
+
+
+createLobbyButton.addEventListener("click", () => {
+  alert("create lobby button")
+})
+
+joinLobbyButton.addEventListener("click", () => {
+  alert("join lobby button")
+})
