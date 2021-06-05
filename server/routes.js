a5b3cbed5d2c722a95beba5c29dde4f164d4c402
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-05 11:47:33 -0400

    express routing cleaned up

--- server/routes.js
+++ server/routes.js
@@ -1,27 +1,14 @@
 const express = require("express")
 const path = require("path")
 
+/**
+ * 
+ * @param {express.Express} app 
+ */
 module.exports = (app) => {
   app.use(express.json())
   app.use(express.raw())
   app.use(express.urlencoded({ extended: true }))
 
-  const cwd = process.cwd() // directory of where server.js is
-  const publicDir = path.join(cwd, "/public")
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(publicDir, "/index.html"))
-  })
-
-  app.get("/lobby", (req, res) => {
-    res.sendFile(path.join(publicDir, "/lobby.html"))
-  })
-
-  app.get("/images/:path", (req, res) => {
-    res.sendFile(path.join(publicDir, `/images/${req.params.path}`))
-  })
-
-  app.get("/:path", (req, res) => {
-    res.sendFile(path.join(publicDir, `/${req.params.path}`))
-  })
+  app.use(express.static("public"))
 }
