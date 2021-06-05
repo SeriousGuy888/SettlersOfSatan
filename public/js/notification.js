0b9dfc09d0b97fb8dfdf7424d4ddaba2a463b75d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-05 14:08:41 -0400

    added in-page notifications so i can stop using alert()

---
+++ public/js/notification.js
@@ -0,0 +1,25 @@
+const notificationContainer = document.querySelector("#notification-container")
+
+const notifyUser = (message) => {
+  const notificationId = `notification-${Date.now()}_${Math.round(Math.random() * 1000)}`
+
+  const notificationDiv = document.createElement("div")
+  notificationDiv.classList.add(["notification"])
+  notificationDiv.id = notificationId
+
+  const notificationText = document.createElement("p")
+  notificationText.classList.add(["notification-content"])
+  notificationText.textContent = message
+
+  const notificationDismissButton = document.createElement("button")
+  notificationDismissButton.classList.add(["notification-dismiss-button"])
+  notificationDismissButton.textContent = "Dismiss"
+  notificationDismissButton.onclick = e => {
+    document.getElementById(notificationId).remove()
+  }
+
+  notificationDiv.appendChild(notificationText)
+  notificationDiv.appendChild(notificationDismissButton)
+
+  notificationContainer.appendChild(notificationDiv)
+}
