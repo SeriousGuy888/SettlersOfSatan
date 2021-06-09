7130947d834c89fc5810c1f7135837d8fd325eec
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-09 18:22:28 -0400

    added canvas background colour

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,8 +1,25 @@
 const gameCanvas = document.querySelector("#game-canvas")
+/**
+ * @type {RenderingContext} ctx
+ */
+const ctx = gameCanvas.getContext("2d")
 
-const canvasFunctions = {
-  setup: () => {
-    gameCanvas.width = 1280
-    gameCanvas.height = 720
-  }
+const canvasWidth = 1280
+const canvasHeight = 720
+
+const canvasFunctions = {}
+
+
+canvasFunctions.setup = () => {
+  gameCanvas.width = canvasWidth
+  gameCanvas.height = canvasHeight
+
+  canvasFunctions.background()
+}
+
+canvasFunctions.background = (colour) => {
+  ctx.beginPath()
+  ctx.rect(0, 0, canvasWidth, canvasHeight)
+  ctx.fillStyle = colour || "#dedede"
+  ctx.fill()
 }
