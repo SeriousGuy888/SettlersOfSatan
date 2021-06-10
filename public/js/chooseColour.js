f7aa4c30f3c88d0ab00ac1b037032dbec8402539
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-10 18:13:51 -0400

    ducking indentation

--- public/js/chooseColour.js
+++ public/js/chooseColour.js
@@ -23,15 +23,14 @@ socket.on("change_allowed_colours", data => {
 })
 
 function colourChoose(button){
-
-    if (allowedColours.includes(button.id)){
+    if(allowedColours.includes(button.id)){
         for (let loopButton of document.getElementsByClassName("colour-button")) loopButton.style.border = "2px solid rgb(0, 0, 0)"
         button.style.border = "4px solid rgb(100, 183, 255)"
 
         socket.emit("choose_colour", {
             colour: button.id,
             lobbyCode: thisLobbyCode
-    })
-    console.log(allowedColours)
+        })
+        console.log(allowedColours)
     }
 }
