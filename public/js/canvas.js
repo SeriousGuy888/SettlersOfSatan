3ad6cfddc2b680295e17dd322302b45d4ab7d16e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-16 15:19:13 -0400

    canvas background clears canvas

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -19,6 +19,7 @@ canvasFunctions.setup = () => {
 }
 
 canvasFunctions.background = (colour) => {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
   ctx.beginPath()
   ctx.rect(0, 0, canvasWidth, canvasHeight)
   ctx.fillStyle = colour || "#dedede"
