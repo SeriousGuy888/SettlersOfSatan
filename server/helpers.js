e82d75ccbba48e0a1163391d77adcd52bc53a6b0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-18 18:36:50 -0400

    added helper function to validate socket requests

--- server/helpers.js
+++ server/helpers.js
@@ -1,5 +1,8 @@
+const { Socket } = require("socket.io")
+const lobbies = require("./lobbies.js")
+const users = require("./users.js")
+
 /**
- * 
  * @param {string} input 
  * @param {boolean} trim 
  * @param {number} maxLength 
@@ -11,4 +14,51 @@ exports.goodifyUserInput = (input, trim, maxLength) => {
   if(maxLength) output = output.slice(0, maxLength)
 
   return output
+}
+
+/**
+ * 
+ * @param {Function} callback 
+ * @param {Socket} socket 
+ * @param {Object} options
+ * @param {boolean} options.requireCallback 
+ * @param {boolean} options.requireUser 
+ * @param {boolean} options.requireLobby 
+ * @param {boolean} options.requireHost 
+ * @returns 
+ */
+exports.validateRequest = (callback, socket, options) => {
+  let returnObj = {}
+
+  if(options) {
+    if(options.requireCallback) {
+      if(!callback) return false
+    }
+    if(options.requireUser) {
+      const user = users.getUser(socket.id)
+      if(!user) return callback("not_logged_in")
+      returnObj.user = user
+    }
+    if(options.requireLobby) {
+      if(returnObj.user) {
+        const { user } = returnObj
+
+        const lobbyCode = user.getLobby()
+        if(!lobbyCode) return callback("not_in_lobby")
+
+        const lobby = lobbies.getLobby(lobbyCode)
+        if(!lobby) return callback("lobby_not_found")
+
+        returnObj.lobby = lobby
+      }
+    }
+    if(options.requireHost) {
+      if(returnObj.user && returnObj.lobby) {
+        const { user, lobby } = returnObj
+        if(lobby.getHost() !== user.id) return callback("no_host_permission")
+      }
+    }
+  }
+
+  return returnObj
 }
