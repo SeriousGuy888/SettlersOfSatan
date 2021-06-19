778c7668e0a4040df384d1a5ff83c41a82398711
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-06-19 16:41:39 -0400

    yellow rectangle

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -87,6 +87,8 @@ canvasFunctions.draw = () => {
       y += hexRadius * 2 - hexRadius / 2
     }
   }
+
+  canvasFunctions.drawInventory()
 }
 
 canvasFunctions.background = (colour) => {
@@ -136,4 +138,15 @@ canvasFunctions.drawHex = (x, y, resource, number) => {
     ctx.textBaseline = "middle"
     ctx.fillText(number.toString(), x, y)
   }
+}
+
+canvasFunctions.drawInventory = () => {
+  const x = 50
+  const y = 50
+
+  ctx.fillStyle = "#efef90"
+
+  ctx.beginPath()
+  ctx.rect(x, y, 300, 500)
+  ctx.fill()
 }
