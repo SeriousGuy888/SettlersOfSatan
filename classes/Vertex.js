4592b9327001927bbaec9e19d6144840bceb776c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-05 16:24:29 -0400

    vertex class can find the hexes that interesect at it

--- classes/Vertex.js
+++ classes/Vertex.js
@@ -1,6 +1,24 @@
 class Vertex {
   constructor(coords) {
     this.coords = coords
+    this.intersectingHexes = this.getIntersectingHexes()
+  }
+
+  getIntersectingHexes() {
+    const { coords } = this
+    const managerHex = [coords[1], coords[0]] // the coords of the hex that manages this vertex
+    
+    let intersectingHexes = [managerHex]
+    if(coords[2] === "north") {
+      intersectingHexes.push([coords[1],      coords[0] - 1])
+      intersectingHexes.push([coords[1] + 1,  coords[0] - 1])
+    }
+    else {
+      intersectingHexes.push([coords[1] - 1,  coords[0] + 1])
+      intersectingHexes.push([coords[1],      coords[0] + 1])
+    }
+
+    return intersectingHexes
   }
 }
 
