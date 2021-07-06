15b8a81d4f246bf6c67b166d50a3312fb312417f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-06 19:04:21 -0400

    display server uptime on server stats page

--- server/routes/server_stats.js
+++ server/routes/server_stats.js
@@ -9,9 +9,11 @@ const { publicDir } = routingUtil
 router.get("/stats", (req, res) => res.sendFile(path.join(publicDir, "/stats.html")))
 router.get("/stats/get", (req, res) => {
   const memoryUsage = process.memoryUsage()
+  const uptime = Math.floor(process.uptime())
 
   res.json({
     memoryUsage,
+    uptime,
   })
 })
 
