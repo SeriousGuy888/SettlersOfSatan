e998df7872a9e1f4864d4584721893b52919b346
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-06 22:20:57 -0400

    fix intersecting hexes thing for vertexes

--- classes/Vertex.js
+++ classes/Vertex.js
@@ -7,16 +7,16 @@ class Vertex {
 
   getIntersectingHexes() {
     const { coords } = this
-    const managerHex = [coords[1], coords[0]] // the coords of the hex that manages this vertex
+    const managerHex = { x: coords.x, y: coords.y } // the coords of the hex that manages this vertex
     
     let intersectingHexes = [managerHex]
-    if(coords[2] === "north") {
-      intersectingHexes.push([coords[1],      coords[0] - 1])
-      intersectingHexes.push([coords[1] + 1,  coords[0] - 1])
+    if(coords.v === "north") {
+      intersectingHexes.push({ x: coords.x,       y: coords.y - 1 })
+      intersectingHexes.push({ x: coords.x + 1,   y: coords.y - 1 })
     }
     else {
-      intersectingHexes.push([coords[1] - 1,  coords[0] + 1])
-      intersectingHexes.push([coords[1],      coords[0] + 1])
+      intersectingHexes.push({ x: coords.x - 1,   y: coords.y + 1 })
+      intersectingHexes.push({ x: coords.x,       y: coords.y + 1 })
     }
 
     return intersectingHexes
