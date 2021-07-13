26c17d8169e4f433910be82caaf9ac1f017833e5
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-13 19:31:13 -0400

    display host icon in player menus for lobby hosts

--- public/js/in_lobby.js
+++ public/js/in_lobby.js
@@ -54,6 +54,7 @@ socket.on("lobby_update", data => {
       modal.style.display = "block"
 
       modalTitle.textContent = user.name
+      user.host && modalTitle.appendChild(hostBadge)
       
       modalButtons.innerHTML = ""
       if(isHost && user.playerId !== playerId) {
