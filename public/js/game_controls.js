095140e344af1b025c793d3ba376fa1b17fbd0d4
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-21 15:04:50 -0400

    split trade interface depending on whose turn it is

--- public/js/game_controls.js
+++ public/js/game_controls.js
@@ -21,35 +21,52 @@ gameControls.road.addEventListener("click", () => setHolding("road"))
 
 const turnControls = document.querySelector("#turn-controls")
 const turnButton = document.querySelector("#end-turn-dice-button")
-const tradeButton = document.querySelector("#trade-button")
+const makeTradeDiv = document.querySelector("#trade-interface-make")
+const makeTradeButton = document.querySelector("#make-trade-button")
+const takeTradeDiv = document.querySelector("#trade-interface-take")
+const takeTradeButton = document.querySelector("#take-trade-button")
 
 const refreshTurnControls = () => {
   if(currentGameData.turn !== currentGameData.me.id) {
     for(let i in gameControls) gameControls[i].disabled = true
 
+    if(currentGameData.turnStage === 1) {
+      makeTradeDiv.style.display = "none"
+      takeTradeDiv.style.display = currentGameData.turnCycle > 2 ? null : "none"
+    }
+
     turnButton.disabled = true
     turnButton.textContent = "It is not your turn..."
-    tradeButton.disabled = true
+
   }
   else {
     turnButton.disabled = false
 
     if(currentGameData.turnStage === 0) {
       for(let i in gameControls) gameControls[i].disabled = true
-      tradeButton.disabled = true
       turnButton.textContent = "Roll Dice"
     }
     else {
       for(let i in gameControls) gameControls[i].disabled = false
-      tradeButton.disabled = (currentGameData.turnCycle <= 2) // disable trade during setup turns
       turnButton.textContent = "End Turn"
+
+      makeTradeDiv.style.display = currentGameData.turnCycle > 2 ? null : "none"
+      takeTradeDiv.style.display = "none"
     }
   }
 }
 
-tradeButton.addEventListener("click", () => {
+makeTradeButton.addEventListener("click", () => {
   socket.emit("perform_game_action", {
     action: "offer_trade"
+  }, (err, data) => {
+    if(err) notifyUser(err)
+  })
+})
+takeTradeButton.addEventListener("click", () => {
+  socket.emit("perform_game_action", {
+    action: "accept_trade",
+    idempotency: currentGameData.trade.idempotency
   }, (err, data) => {
     if(err) notifyUser(err)
   })
