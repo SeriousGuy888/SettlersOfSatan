71d8500e7ab549133c128b04be94e9376b0db0c0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-22 14:39:17 -0400

    creation of resource card displays now managed with javascript

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,14 +1,49 @@
+const resourceCardsDiv = document.querySelector("#resource-cards")
 const resourceDivs = {
-  bricks: document.querySelector("#resource-bricks"),
-  lumber: document.querySelector("#resource-lumber"),
-  wool: document.querySelector("#resource-wool"),
-  wheat: document.querySelector("#resource-wheat"),
-  ore: document.querySelector("#resource-ore"),
+  bricks: "resource-bricks",
+  lumber: "resource-lumber",
+  wool: "resource-wool",
+  wheat: "resource-wheat",
+  ore: "resource-ore",
 }
 
 const refreshResourceCards = () => {
+  if(!resourceCardsDiv.childElementCount) {
+    const frag = document.createDocumentFragment()
+
+    for(let resourceName in resourceDivs) {
+      const resourceCard = document.createElement("div")
+      resourceCard.className = "resource-card"
+      resourceCard.id = resourceDivs[resourceName]
+
+      const img = document.createElement("img")
+      img.src = `/images/resource_cards/${resourceName}.png`
+      img.alt = resourceName
+
+      const h3 = document.createElement("h3")
+      h3.className = "resource"
+
+      resourceCard.appendChild(img)
+      resourceCard.appendChild(h3)
+      frag.appendChild(resourceCard)
+    }
+
+    const developmentCardToggle = document.createElement("div")
+    developmentCardToggle.className = "resource-card"
+    developmentCardToggle.id = "development-card-toggle"
+    developmentCardToggle.textContent = "Toggle Development Cards"
+    developmentCardToggle.onclick = () => {
+      const developmentCardListDiv = document.querySelector("#development-card-list")
+      if(!developmentCardListDiv.style.display) developmentCardListDiv.style.display = "none"
+      else developmentCardListDiv.style.display = null
+    }
+    frag.appendChild(developmentCardToggle)
+
+    resourceCardsDiv.appendChild(frag)
+  }
+
   for(let divKey in resourceDivs) {
-    const amtDisplay = resourceDivs[divKey].querySelector(".resource")
+    const amtDisplay = document.getElementById(resourceDivs[divKey]).querySelector(".resource")
     amtDisplay.textContent = currentGameData.me.resources[divKey]
   }
 }
