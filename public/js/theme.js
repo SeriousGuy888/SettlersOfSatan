c641c3e3e8fa1bd0f20aaf0fabaf71794e6bcdc5
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-23 18:22:04 -0400

    dark theme toggle in contributors page

--- public/js/theme.js
+++ public/js/theme.js
@@ -3,12 +3,12 @@ const darkThemeToggle = document.querySelector("#dark-theme-toggle")
 const setDarkTheme = (enabled) => {
   localStorage.setItem("darkTheme", enabled)
   document.body.classList.toggle("dark-theme", enabled)
-  darkThemeToggle.textContent = enabled ? "🌞 Light Theme" : "🌛 Dark Theme"
+  if(darkThemeToggle) darkThemeToggle.textContent = enabled ? "🌞 Light Theme" : "🌛 Dark Theme"
 }
 const getDarkTheme = () => localStorage.getItem("darkTheme") === "true" // localstorage only takes strings :(
 setDarkTheme(getDarkTheme())
 
-darkThemeToggle.addEventListener("click", () => {
+if(darkThemeToggle) darkThemeToggle.addEventListener("click", () => {
   let enabled = getDarkTheme()
   setDarkTheme(!enabled)
 })
