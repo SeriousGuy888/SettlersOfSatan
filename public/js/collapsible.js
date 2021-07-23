aebb38c416645a74e60e9f220fbea310714bf86f
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-22 20:44:08 -0400

    trade panel is a flexbox again

--- public/js/collapsible.js
+++ public/js/collapsible.js
@@ -6,6 +6,6 @@ for(i = 0; i < allCollapsibles.length; i++) {
     this.classList.toggle("collapsible-active")
     const content = this.nextElementSibling
     if(content.style.display !== "none") content.style.display = "none"
-    else content.style.display = "block"
+    else content.style.display = null
   })
 }
