9e5932dd9566fe856ccd165b7c338e8e126a11d1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-07-30 18:14:34 -0400

    add getAdjacentVertexes() method to Hex class

--- classes/Hex.js
+++ classes/Hex.js
@@ -24,11 +24,32 @@ class Hex {
 
     const xOffset = y % 2 === 0 ? -1 : 1
     adjacentHexes.push(
-      {x: x + xOffset, y: y + 1},
-      {x: x + xOffset, y: y - 1}
+      { x: x + xOffset, y: y + 1 },
+      { x: x + xOffset, y: y - 1 }
     )
 
     return adjacentHexes
+  }
+
+  getAdjacentVertexes() {
+    const { coords } = this
+    const { x, y } = coords
+    
+    const adjacentVertexes = []
+    adjacentVertexes.push(
+      { x, y,        v: "north" },
+      { x, y,        v: "south" },
+      { x, y: y - 1, v: "south" },
+      { x, y: y + 1, v: "north" },
+    )
+
+    const xOffset = y % 2 === 0 ? -1 : 1
+    adjacentVertexes.push(
+      { x: x + xOffset, y: y - 1, v: "south" },
+      { x: x + xOffset, y: y + 1, v: "north" }
+    )
+
+    return adjacentVertexes
   }
 
   setInvisible(invisible) {
