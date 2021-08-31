5a081743ca09fb78f659b72e03f8650c15fc7534
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-31 13:00:49 -0400

    reimplemented edges with vue

--- src/components/classes/Edge.js
+++ src/components/classes/Edge.js
@@ -2,14 +2,16 @@ class Edge {
   constructor(that, coordsArr, data) {
     this.that = that    
     this.coordsArr = coordsArr
-    Object.assign(this, that)
+    Object.assign(this, data)
+    this.radius = this.that.hexRadius / 6
   }
 
   render() {
-    const { coordsArr, that } = this
+    const { coordsArr, that, radius } = this
+    const { board, ctx, drawSelectCircle, holding, state } = that
 
     const getVertex = coords => {
-      return that.board.vertexes.filter(vert => {
+      return board.vertexes.filter(vert => {
         const vCoords = vert.coords
         return vCoords.x === coords.x && vCoords.y === coords.y && vCoords.v === coords.v
       })[0]
@@ -18,18 +20,16 @@ class Edge {
     const vertA = getVertex(coordsArr[0])
     const vertB = getVertex(coordsArr[1])
     if(!vertA || !vertB) return
-
     this.xPos = (vertA.xPos + vertB.xPos) / 2
     this.yPos = (vertA.yPos + vertB.yPos) / 2
 
     if(this.road) {
-      const road = this.road
-      const colour = canvasFunctions.getPlayer(road).colour
+      const roadOwner = this.road
+      const colour = state.game.players[roadOwner].colour
 
       ctx.beginPath()
       ctx.moveTo(vertA.xPos, vertA.yPos)
       ctx.lineTo(vertB.xPos, vertB.yPos)
-
       ctx.lineWidth = 14
       ctx.strokeStyle = "#fff"
       ctx.stroke()
@@ -39,39 +39,34 @@ class Edge {
       ctx.closePath()
     }
 
-    // if(holding === "road") {
-    //   if(this.road) return
-    //   if(!this.allowPlacement) return
-
-    //   ctx.fillStyle = "#08f8"
-    //   ctx.beginPath()
-    //   ctx.arc(this.xPos, this.yPos, this.getDimensions().width / 2, 0, 2 * Math.PI)
-    //   ctx.fill()
-
-    // }
-  }
-  
-  getDimensions() {
-    return selectionTargetDims
+    if(holding === "road") {
+      if(this.road) return
+      if(!this.allowPlacement) return
+      drawSelectCircle(this.xPos, this.yPos, radius)
+    }
   }
 
-  // onClick() {
-  //   if(!this.allowPlacement) return
-  //   if(!this.isHovered(true)) return
-  //   if(this.road) return
-  //   if(holding !== "road") return
+  isHovered(mousePos) {
+    const { that, xPos, yPos, radius } = this
+    return that.getDist(mousePos.x, mousePos.y, xPos, yPos) <= radius
+  }
 
-  //   console.log(`Clicked on edge ${JSON.stringify(this.coordsArr)}`)
+  click() {
+    const { that } = this
+    const { holding, setHolding, state } = that
 
-  //   socket.emit("perform_game_action", {
-  //     action: "place_road",
-  //     coordsArr: this.coordsArr,
-  //   }, (err, data) => {
-  //     if(err) notifyUser(err)
-  //   })
+    if(!this.allowPlacement) return
+    if(this.road) return
+    if(holding !== "road") return
 
-  //   if(currentGameData.roadBuilding < 2) setHolding(null)
-  // }
+    console.log(`Clicked on edge ${JSON.stringify(this.coordsArr)}`)
+    socket.emit("perform_game_action", {
+      action: "place_road",
+      coordsArr: this.coordsArr,
+    }, () => {})
+
+    if(state.game.roadBuilding < 2) setHolding(null)
+  }
 }
 
 module.exports = Edge
