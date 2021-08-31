1100f9c529877d381aeff1bee62b8c30565f66da
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-08-31 14:19:45 -0400

    reimplemented trade menu with vue

--- src/components/TradePanel.vue
+++ src/components/TradePanel.vue
@@ -3,33 +3,42 @@
     <div v-if="tradingAllowed" id="trade-interface">
       <p id="trade-offerer-name">{{ state.game.players[state.game.turn].name }}</p>
       <br>
-      <select v-model="tradeMode" id="trade-taker-select">
+      <select v-model="tradeMode" :disabled="tradeMode === 'discard'">
         <option value="humans">Humans</option>
         <option value="stockpile">Bank</option>
         <option value="discard" style="display: none;">Discard</option>
       </select>
 
-      <TradePanelInputs @updateAmounts="updateAmounts('offerer', $event)" />
+      <TradePanelInputs
+        @updateAmounts="updateAmounts('offerer', $event)"
+        :tradeMode="tradeMode"
+      />
       <div class="trade-icon">
         <img :src="getTradeIcon()" alt="trade">
       </div>
-      <TradePanelInputs @updateAmounts="updateAmounts('taker', $event)" :rightSide="true" />
+      <TradePanelInputs
+        @updateAmounts="updateAmounts('taker', $event)"
+        v-if="this.tradeMode !== 'discard'"
+        :tradeMode="tradeMode"
+        :rightSide="true"
+      />
     </div>
 
     <div v-if="tradingAllowed">
       <div v-if="state.game && state.player && (state.game.turn === state.player.id || requiredDiscardCount)">
-        <button v-if="state.game.trade.offer">Cancel Trade Offer</button>
-        <button v-else-if="tradeMode === 'humans'">Propose Trade</button>
-        <button v-else-if="tradeMode === 'stockpile'">Trade With Harbour</button>
+        <button v-if="state.game.trade.offer" @click="cancelTradeOffer()">Cancel Trade Offer</button>
+        <button v-else-if="tradeMode === 'humans'" @click="makeTrade('offer_trade')">Propose Trade</button>
+        <button v-else-if="tradeMode === 'stockpile'" @click="makeTrade('harbour_trade')">Trade With Harbour</button>
         <button
           v-else-if="tradeMode === 'discard'"
           :disabled="requiredDiscardCount !== totalSelectedOfferer"
+          @click="makeTrade('discard_cards')"
         >
-          Discard {{ requiredDiscardCount }} cards ({{ totalSelectedOfferer }} required)
+          Discard {{ requiredDiscardCount }} cards ({{ totalSelectedOfferer }} selected)
         </button>
       </div>
       <div v-else>
-        <button v-if="state.game.trade.offer">Accept Trade</button>
+        <button v-if="state.game.trade.offer" @click="acceptTrade()">Accept Trade</button>
         <p v-else>No trade offer right now...</p>
       </div>
     </div>
@@ -50,7 +59,10 @@ export default {
     return {
       state: this.$store.state,
       tradeMode: "humans",
-      amounts: {},
+      amounts: {
+        offerer: {},
+        taker: {},
+      },
     }
   },
   methods: {
@@ -63,12 +75,25 @@ export default {
     updateAmounts(column, newData) {
       this.amounts[column] = newData
     },
+    cancelTradeOffer() {
+      socket.emit("perform_game_action", { action: "cancel_trade" }, () => {})
+    },
+    makeTrade(action) {
+      socket.emit("perform_game_action", {
+        action,
+        offer: this.amounts,
+      }, () => {})
+    },
+    acceptTrade() {
+      socket.emit("perform_game_action", {
+        action: "accept_trade",
+        idempotency: state.game.trade.idempotency,
+      }, () => {})
+    },
   },
   computed: {
     requiredDiscardCount() {
-      let count = this.game.discardingPlayers[this.state.player.id]
-      this.tradeMode = count ? "discard" : "humans"
-      return count
+      return this.state.game.discardingPlayers[this.state.player.id]
     },
     tradingAllowed() {
       return (
@@ -85,6 +110,17 @@ export default {
     totalSelectedOfferer() {
       return Object.values(this.amounts.offerer).reduce((acc, cur) => acc + cur)
     },
-  }
+  },
+  mounted() {
+    setInterval(() => {
+      if(this.requiredDiscardCount) {
+        this.tradeMode = "discard"
+      } else {
+        if(this.tradeMode === "discard") {
+          this.tradeMode = "humans"
+        }
+      }
+    }, 1000)
+  },
 }
 </script>
--- src/components/TradePanelInputs.vue
+++ src/components/TradePanelInputs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="trade-column">
     <div
-      v-for="resource in resources"
+      v-for="resource in Object.keys(amounts)"
       :key="resource"
       id="trade-taker-inputs"
       class="trade-column"
@@ -11,7 +11,13 @@
         :class="{ 'trade-amount-div-right': rightSide }"
       >
         <img :src="getResourceIcon(resource)" :alt="resource">
-        <input v-model="amounts[resource]" @change="updateAmounts()" type="number" min="0">
+        <input
+          v-model="amounts[resource]"
+          @change="updateAmounts()"
+          :disabled="state.game.trade.offer || (tradeMode === 'stockpile' && total > 0 && !amounts[resource])"
+          type="number"
+          min="0"
+        >
       </div>
     </div>
   </div>
@@ -21,18 +27,24 @@
 export default {
   props: {
     rightSide: Boolean,
+    tradeMode: String,
   },
   data() {
     return {
-      resources: [
-        "bricks",
-        "lumber",
-        "wool",
-        "wheat",
-        "ore",
-      ],
-      amounts: {}
+      state: this.$store.state,
+      amounts: {
+        "bricks": 0,
+        "lumber": 0,
+        "wool": 0,
+        "wheat": 0,
+        "ore": 0,
+      }
     }
+  },
+  computed: {
+    total() {
+      return Object.values(this.amounts).reduce((acc, cur) => acc + cur)
+    },
   },
   methods: {
     getResourceIcon(resource) {
@@ -41,6 +53,19 @@ export default {
     updateAmounts() {
       this.$emit("updateAmounts", this.amounts)
     },
+    wipe() {
+      for(let i in this.amounts) {
+        this.amounts[i] = 0
+      }
+    },
+  },
+  mounted() {
+    this.updateAmounts()
+    setInterval(() => {
+      if(this.tradeMode === "stockpile" && Object.values(this.amounts).filter(e => e > 0).length > 1) {
+        this.wipe()
+      }
+    }, 1000)
   },
 }
 </script>
