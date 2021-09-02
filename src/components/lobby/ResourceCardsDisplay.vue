7aec90a7aafbee1f867f19bbc842a042bc876093
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-09-01 21:27:13 -0400

    resource cards styles moved into vue component

--- src/components/lobby/ResourceCardsDisplay.vue
+++ src/components/lobby/ResourceCardsDisplay.vue
@@ -35,3 +35,47 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+#resource-cards {
+  display: grid;
+  gap: 3px;
+  grid: 1fr 1fr 1fr / 1fr 1fr;
+  margin-top: 1em;
+}
+.resource-card {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  border-radius: 5px;
+  text-align: center;
+  align-items: center;
+  margin: 0.1em;
+}
+.resource-card.green-pulse { animation: green-pulse 1s; }
+.resource-card.red-pulse { animation: red-pulse 1s; }
+.resource-card > img {
+  width: 2.5em;
+  height: 2.5em;
+  padding: 0.2em;
+}
+.resource-card > .trade-amount-selection-div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+.resource-card > .trade-amount-selection-div > .trade-amount-input {
+  width: 2em;
+  height: 2em;
+  background-color: #0000;
+  border: 3px solid #bb9662;
+  border-radius: 5px;
+  padding: 0;
+  text-align: center;
+  font-size: 1em;
+}
+.resource-card > .resource {
+  text-align: right;
+}
+</style>
