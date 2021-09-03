de1d73fd02cc826f7336c9dcb7482169e2486e96
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-09-03 13:45:28 -0400

    display piece counts on buttons

--- src/components/lobby/CenterColumnControls.vue
+++ src/components/lobby/CenterColumnControls.vue
@@ -1,38 +1,36 @@
 <template>
   <div id="game-controls" class="button-group" v-if="state.player">
     <button
-      class="game-control-button"
       :class="{ active: holding === 'settlement' }" 
       :disabled="!state.player.enableControls.settlement"
       @click="setHolding('settlement')"
     >
       <img src="/images/game_controls/settlement.svg" alt="Settlement">
+      <p>{{ state.player.inventory.settlements }}</p>
     </button>
     <button
-      class="game-control-button"
       :class="{ active: holding === 'city' }"
       :disabled="!state.player.enableControls.city"
       @click="setHolding('city')"
     >
       <img src="/images/game_controls/city.svg" alt="City">
+      <p>{{ state.player.inventory.cities }}</p>
     </button>
     <button
-      class="game-control-button"
       :class="{ active: holding === 'road' }"
       :disabled="!state.player.enableControls.road"
       @click="setHolding('road')"
     >
       <img src="/images/game_controls/road.svg" alt="Road">
+      <p>{{ state.player.inventory.roads }}</p>
     </button>
     <button
-      class="game-control-button"
       :disabled="!state.player.enableControls.developmentCard"
       @click="buyDevelopmentCard()"
     >
       <img src="/images/game_controls/development_card.svg" alt="Development Card">
     </button>
   </div>
-  <p id="inv-list" v-if="state.player">Settlements: {{ state.player.inventory.settlements }}, Roads: {{ state.player.inventory.roads }}, Cities: {{ state.player.inventory.cities }}</p>
 </template>
 
 <script>
@@ -71,3 +69,40 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+  #game-controls {
+    display: flex;
+    align-content: flex-start;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 3px;
+    padding: 0 0.5em;
+  }
+  #game-controls > button {
+    display: flex;
+    flex: 1 0 4em;
+    height: 4em;
+    padding: 1em;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  #game-controls > button.active {
+    border: 2px solid var(--theme-select-1);
+    background-color: var(--theme-select-2);
+  }
+  #game-controls > button > img {
+    height: 2.5em;
+    width: 2.5em;
+  }
+  #game-controls > button > p {
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+    width: 2em;
+    line-height: 2em;
+    border: 2px solid var(--theme-text);
+    border-radius: 50%;
+  }
+</style>
