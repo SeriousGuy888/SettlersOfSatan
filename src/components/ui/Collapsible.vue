081937b91f004c9fc1cb8cb0a2ae726528c853da
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-09-04 00:17:38 -0400

    collapsible styles moved into component file

--- src/components/ui/Collapsible.vue
+++ src/components/ui/Collapsible.vue
@@ -24,3 +24,31 @@ export default {
   },
 }
 </script>
+
+<style>
+/* stolen from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible_symbol lol */
+.collapsible {
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 0.2em 1em;
+  width: 100%;
+  outline: none;
+  font-size: 1.1em;
+  text-align: left;
+  border: 3px solid var(--theme-game-display-1);
+  background-color: var(--theme-game-display-2);
+
+  margin-top: 0.5em;
+}
+.collapsible-active,
+.collapsible:hover {
+  background-color: var(--theme-game-display-1);
+}
+.collapsible::after {
+  content: "\25BC";
+  float: right;
+  margin-left: 5px;
+}
+.collapsible-active::after { content: "\25B2"; }
+.collapsible-active { border-radius: 5px 5px 0 0; }
+</style>
