7311f24befe276ae0a529fa4132b13ce0c99dd60
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-09-05 13:08:27 -0400

    goodify developmentcard indentation

--- server/classes/DevelopmentCard.js
+++ server/classes/DevelopmentCard.js
@@ -1,42 +1,41 @@
 const lobbies = require("../lobbies.js")
 
 class DevelopmentCard {
-    constructor(type, lobbyId, playerId, cycleBought) {
-      this.type = type
-      this.victoryPoint = ["library", "market", "chapel", "great hall", "university"].includes(type)
-      this.lobbyId = lobbyId
-      this.playerId = playerId
-      this.cycleBought = cycleBought
-      this.id = Date.now() + Math.random()
-      
-      if (this.type == "knight") this.knightType = Math.floor(Math.random() * 3) + 1
-      else this.knightType = null
-    }
+  constructor(type, lobbyId, playerId, cycleBought) {
+    this.type = type
+    this.victoryPoint = ["library", "market", "chapel", "great hall", "university"].includes(type)
+    this.lobbyId = lobbyId
+    this.playerId = playerId
+    this.cycleBought = cycleBought
+    this.id = Date.now() + Math.random()
+    
+    if (this.type == "knight") this.knightType = Math.floor(Math.random() * 3) + 1
+    else this.knightType = null
+  }
 
-    use() {
-      let currentGame = lobbies.getLobby(this.lobbyId).game
-      let player = currentGame.players[this.playerId]
-      if(currentGame.turnCycle != this.cycleBought){
-        player.inventory.removeDevelopmentCard(this)
-        switch(this.type) {
-          case "knight":
-            currentGame.robbing = true
-            break
-          
-          case "library": case "market": case "chapel": case "great hall": case "university":
-            player.points++
-            break
-
-          case "road building":
-            currentGame.roadBuilding = 2
-            // currentGame.processAction(this.playerId, { action: "place_road" })
-            break
-        }
-      }
-      else {
-        currentGame.printChatErr("You can't use a development card on the turn you bought it", this.playerId)
+  use() {
+    let currentGame = lobbies.getLobby(this.lobbyId).game
+    let player = currentGame.players[this.playerId]
+    if(currentGame.turnCycle != this.cycleBought){
+      player.inventory.removeDevelopmentCard(this)
+      switch(this.type) {
+        case "knight":
+          currentGame.robbing = true
+          break
+        case "library": case "market": case "chapel": case "great hall": case "university":
+          player.points++
+          break
+        case "road building":
+          currentGame.roadBuilding = 2
+          break
+        case "year of plenty":
+          break
       }
     }
+    else {
+      currentGame.printChatErr("You can't use a development card on the turn you bought it", this.playerId)
+    }
+  }
 }
 
 module.exports = DevelopmentCard
